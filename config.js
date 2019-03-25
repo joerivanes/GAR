fb2beae27d11adc465a535cf7c4e5c046b37438b
Linda van den Brink <l.vandenbrink@geonovum.nl>
2019-03-25 16:48:32 +0100

    config back to werkversie

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var respecConfig = {
-  specStatus: "GN-CV",
+  specStatus: "GN-WV",
   specType: "IM",
   //canonicalURI: "WV",
-  publishDate: "2019-03-25",
+  //publishDate: "2019-03-25",
   maxTocLevel: 3,
   editors: [
     {
@@ -17,8 +17,8 @@ var respecConfig = {
   shortName: "gar",
   pubDomain: "bro",
   github: "https://github.com/BROprogramma/GAR",
-  //previousPublishDate: "2019-03-25",
-  //previousStatus: "GN-CV",
+  previousPublishDate: "2019-03-25",
+  previousStatus: "GN-CV",
   //localBiblio: {
   //  "ISO19136": {
   //    "href": "https://www.iso.org/standard/32554.html",
